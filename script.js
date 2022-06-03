f6ae47619165188960e12e110c55ff33d8d57e16
Pedro Nunes <pedro.icn88@gmail.com>
2022-06-03 17:29:41 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // gera cores aleatórias
 const collorsBall = document.getElementsByClassName('ball');
-const colors = document.querySelector('#colors')
-const rgbColor = document.querySelector('#rgb-color')
-const answer = document.querySelector('#answer')
+const colors = document.querySelector('#colors');
+const rgbColor = document.querySelector('#rgb-color');
+const answer = document.querySelector('#answer');
+const resetButton = document.querySelector('#reset-game')
 
 window.onload = randomColorGenerate;
 
@@ -16,7 +17,7 @@ function randomColorGenerate() {
   }
   let initialRamdom = Math.floor(Math.random() * 5 + 1);
   rgbColor.innerText = collorsBall[initialRamdom].style.backgroundColor;
-  
+  answer.innerText = 'Escolha uma cor';
 }
 
 // checa se a cor escolhida é a mesma e responde se acertou ou errou
@@ -28,4 +29,12 @@ function colorCheck(event) {
   } else {
     answer.innerText = "Errou! Tente novamente!";
   }
-}  
+} 
+
+
+// cria botão para resetar cores
+resetButton.addEventListener('click', resetColors);
+function resetColors() {
+  window.onload()
+ 
+}
