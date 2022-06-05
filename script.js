5e8dd3903400f56859ca36927690e79d3ca3f4d9
Pedro Nunes <pedro.icn88@gmail.com>
2022-06-05 17:22:30 -0300

    Adiciona estilo ao projeto

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const colors = document.querySelector('#colors');
 const rgbColor = document.querySelector('#rgb-color');
 const answer = document.querySelector('#answer');
 const resetButton = document.querySelector('#reset-game')
+const pontos = document.querySelector('#score')
+
 
 window.onload = randomColorGenerate;
 
@@ -18,23 +20,41 @@ function randomColorGenerate() {
   let initialRamdom = Math.floor(Math.random() * 5 + 1);
   rgbColor.innerText = collorsBall[initialRamdom].style.backgroundColor;
   answer.innerText = 'Escolha uma cor';
+  
 }
 
 // checa se a cor escolhida é a mesma e responde se acertou ou errou
 colors.addEventListener('click', colorCheck)
 
 function colorCheck(event) {
+  let pontuacao = 0;
   if(event.target.style.backgroundColor === rgbColor.innerText) {
     answer.innerText = "Acertou!"
+    pontuacao += 3
+    pontuacao = pontuacao + "";
+    pontos.innerHTML = pontuacao
+    
   } else {
     answer.innerText = "Errou! Tente novamente!";
   }
+  
+ 
 } 
 
 
 // cria botão para resetar cores
 resetButton.addEventListener('click', resetColors);
 function resetColors() {
-  window.onload()
+  randomColorGenerate()
  
 }
+
+// function addScore() {
+//   localStorage.setItem('myPoints', JSON.stringify(pontos.innerText))
+// }
+// function addScore()
+// function getScore() {
+//   const myPoints = JSON.parse(localStorage.getItem('myPoints'));
+//   pontos.innerText = myPoints;
+// }
+// getScore()
