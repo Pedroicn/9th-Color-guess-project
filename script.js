a53e6258dcc4610efb21cd55ffd1d401cf5173a3
Pedro Nunes <pedro.icn88@gmail.com>
2022-11-01 13:25:21 -0300

    Corrige erros de lint

--- script.js
+++ script.js
@@ -3,52 +3,41 @@ const collorsBall = document.getElementsByClassName('ball');
 const colors = document.querySelector('#colors');
 const rgbColor = document.querySelector('#rgb-color');
 const answer = document.querySelector('#answer');
-const resetButton = document.querySelector('#reset-game')
-const pontos = document.querySelector('#score')
-
-
-window.onload = randomColorGenerate;
+const resetButton = document.querySelector('#reset-game');
+const pontos = document.querySelector('#score');
 
 function randomColorGenerate() {
-  
-  for(let i = 0; i < collorsBall.length; i += 1) {
-    let r = Math.round(Math.random() * 255 + 1);
-    let g = Math.round(Math.random() * 255 + 1);
-    let b = Math.round(Math.random() * 255 + 1);
-    collorsBall[i].style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+  for (let i = 0; i < collorsBall.length; i += 1) {
+    const r = Math.round(Math.random() * 255 + 1);
+    const g = Math.round(Math.random() * 255 + 1);
+    const b = Math.round(Math.random() * 255 + 1);
+    collorsBall[i].style.backgroundColor = `rgb(${r},${g},${b})`;
   }
-  let initialRamdom = Math.floor(Math.random() * 5 + 1);
+  window.onload = randomColorGenerate;
+  const initialRamdom = Math.floor(Math.random() * 5 + 1);
   rgbColor.innerText = collorsBall[initialRamdom].style.backgroundColor;
   answer.innerText = 'Escolha uma cor';
-  
 }
 
 // checa se a cor escolhida é a mesma e responde se acertou ou errou
-colors.addEventListener('click', colorCheck)
-
+let pontuacao = 0;
 function colorCheck(event) {
-  let pontuacao = 0;
-  if(event.target.style.backgroundColor === rgbColor.innerText) {
-    answer.innerText = "Acertou!"
-    pontuacao += 3
-    pontuacao = pontuacao + "";
-    pontos.innerHTML = pontuacao
-    
+  if (event.target.style.backgroundColor === rgbColor.innerText) {
+    answer.innerText = 'Acertou!';
+    pontuacao += 3;
+    pontos.innerHTML = pontuacao;
   } else {
-    answer.innerText = "Errou! Tente novamente!";
+    answer.innerText = 'Errou! Tente novamente!';
   }
-  
- 
-} 
-
+}
+colors.addEventListener('click', colorCheck);
 
 // cria botão para resetar cores
-resetButton.addEventListener('click', resetColors);
 function resetColors() {
-  randomColorGenerate()
- 
+  randomColorGenerate();
 }
 
+resetButton.addEventListener('click', resetColors);
 // function addScore() {
 //   localStorage.setItem('myPoints', JSON.stringify(pontos.innerText))
 // }
